877f3c3d855a693541afed79001d58f5fb998ee7
Alysson Torres <alysson-b@hotmail.com>
2025-07-10 10:49:27 -0300

    adsdas

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@200..700&family=Passion+One:wght@400;700;900&family=Sriracha&family=Work+Sans:ital@0;1&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilos.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+
+    </header>
+
+    <main class="principal">
+
+    </main>
+    <footer class="rodape">
+
+    </footer>
+
+</body>
+
+</html>
