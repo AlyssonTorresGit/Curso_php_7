7dbc01ec74fa9aa26a9b083b872f79da82030ca0
Alysson Torres <alysson-b@hotmail.com>
2025-07-11 10:44:31 -0300

    jygnugkj

---
+++ exercicio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@200..700&family=Passion+One:wght@400;700;900&family=Sriracha&family=Work+Sans:ital@0;1&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/exercicio.css">
+    <link rel="stylesheet" href="recursos/css/estilos.css">
+    <title>Exercícios</title>
+</head>
+
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP 7</h1>
+        <h2>Visualização de exercicios
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Semformatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                 <div class="modulo verde">
                     <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="exercicio.php">Exercício A</a></li>
                         <li><a href="#">Exercício B</a></li>
                         <li><a href="#">Exercício C</a></li>
                     </ul>
@@ -99,7 +99,7 @@
 
     </main>
     <footer class="rodape">
-        COD3R & ALUNOS 2025
+        COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 
 </body>
