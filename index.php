d5dc9582b1eb432f54b89d07a84f51fdcd4ea466
Alysson Torres <alysson-b@hotmail.com>
2025-07-29 10:03:04 -0300

    aula 4

--- index.php
+++ index.php
@@ -1,59 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@200..700&family=Passion+One:wght@400;700;900&family=Sriracha&family=Work+Sans:ital@0;1&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="recursos/css/estilos.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
     <title>Curso PHP</title>
 </head>
-
 <body>
     <header class="cabecalho">
-        <h1>Curso PHP 7</h1>
-        <h2>Índices dos Exercícios</h2>
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
     </header>
-
     <main class="principal">
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Básico</h3>
                     <ul>
-                        <li><a href="exercicio.php?dir=basico&file=ola">
-                                Ola PHP!
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">
+                                Olá PHP
                             </a>
                         </li>
-                        <li><a href="exercicio.php?dir=basico&file=html">
-                                integração HTML
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">
+                                Integração HTML
                             </a>
                         </li>
                         <li>
                             <a href="exercicio.php?dir=basico&file=css">
-                                integração CSS
+                                Integração CSS
                             </a>
                         </li>
                         <li>
                             <a href="exercicio.php?dir=basico&file=comentarios">
-                                Comentarios PHP
+                                Comentários PHP
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">
+                                Desafio
                             </a>
                         </li>
                     </ul>
                 </div>
-
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">
+                                Tipo Inteiro
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=float">
+                                Tipo Float
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=aritmeticas">
+                                Op. Aritméticas
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=desafio_precedendia">
+                                Desafio Precedência
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=string">
+                                Tipo String
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=desafio_string">
+                                Desafio String
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=booleano">
+                                Tipo Booleano
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=conversoes">
+                                Conversões
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
-
     </main>
     <footer class="rodape">
         COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
-
 </body>
-
 </html>
