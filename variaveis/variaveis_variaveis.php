57eec1f0762814e141274b566e97a3a8869262ef
Alysson Torres <alysson-b@hotmail.com>
2025-07-30 14:08:49 -0300

    YTITYI

---
+++ variaveis/variaveis_variaveis.php
@@ -0,0 +1,8 @@
+<div class="constantes">Contantes</div>
+<?php
+define('NOME_DA_CONSTANTE', valor) ou CONST NOME_DA_CONSTANTE;
+
+$valorvariavel = 2.8;
+
+define('NOVISSIMA_TAXA', $valorvariavel);
+echo '<br>' . NOVISSIMA_TAXA;
