4cdbdb98a3164754c1ec06e6004007831f32ad0b
Alysson Torres <alysson-b@hotmail.com>
2025-08-14 10:30:19 -0300

    asdsa

--- repeticoes/break_continue.php
+++ repeticoes/break_continue.php
@@ -2,28 +2,44 @@
 
 <?php
 $cont = 16;
-for(;;) {
+for (;;) {
     echo "$cont <br>";
     $cont++;
-    if($cont > 20) {
+    if ($cont > 20) {
+        break;
+    }
+}
+
+for (;;) {
+    echo "$cont <r>";
+    $cont++;
+    if ($cont > 20) {
         break;
     }
 }
 
 echo "<br><hr>";
 
-for(;;) {
+for (;;) {
     $cont++;
-    if($cont % 2 === 1) continue;
+    if ($cont % 2 === 1) continue;
     echo "$cont <br>";
-    if($cont >= 30) {
+    if ($cont >= 30) {
+        break;
+    }
+}
+for (;;) {
+    $cont++;
+    if ($cont % 2 === 1) continue; //o % é o operador módulo da operação
+    echo "$cont <br>";
+    if ($cont >= 30) {
         break;
     }
 }
 
-foreach(array(1,2,3,4,5,6) as $valor) {
-    if($valor === 5) break;
-    if($valor % 2 === 0) continue;
+foreach (array(1, 2, 3, 4, 5, 6) as $valor) {
+    if ($valor === 5) break;
+    if ($valor % 2 === 0) continue;
     echo "$valor <br>";
 }
 
